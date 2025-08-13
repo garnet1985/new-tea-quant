bba427d4e84bd470392ea297c9e4c65096c06483
Garnet Xin <xin_garnet@foxmail.com>
2025-08-13 16:43:57 +0800

    complete to_qfq data comparision, the error is limited to 5%

--- app/data_source/data_source_service.py
+++ app/data_source/data_source_service.py
@@ -1,4 +1,6 @@
 from datetime import date as date_type
+import pprint
+from loguru import logger
 
 class DataSourceService:
     @staticmethod
@@ -26,7 +28,96 @@ class DataSourceService:
 
     @staticmethod
     def to_qfq(k_lines: list, qfq_factors: list):
-        print(qfq_factors)
-        pass
+        """
+        计算前复权价格
+        
+        Args:
+            k_lines: K线数据列表，包含 date, open, close, highest, lowest 等字段
+            qfq_factors: 复权因子列表，包含 date, qfq 等字段
+            
+        Returns:
+            list: 前复权后的K线数据列表
+        """
+        if not k_lines or not qfq_factors:
+            return k_lines
+            
+        # 确保因子按日期升序
+        sorted_factors = sorted(qfq_factors, key=lambda x: x['date'])
+        
+        # 处理每条K线
+        for k_line in k_lines:
+            # 保存原始值到raw属性
+            k_line['raw'] = {
+                'open': k_line.get('open'),
+                'close': k_line.get('close'),
+                'highest': k_line.get('highest'),
+                'lowest': k_line.get('lowest')
+            }
+            
+            # 获取当前K线的日期
+            current_date = k_line.get('date')
+            if not current_date:
+                continue
+                
+            # 查找对应的复权因子（使用已排序的因子）
+            qfq_factor = DataSourceService._find_qfq_factor(current_date, sorted_factors)
+            
+            if qfq_factor is not None:
+                # 计算前复权价格
+                k_line['open'] = k_line['raw']['open'] * qfq_factor if k_line['raw']['open'] else None
+                k_line['close'] = k_line['raw']['close'] * qfq_factor if k_line['raw']['close'] else None
+                k_line['highest'] = k_line['raw']['highest'] * qfq_factor if k_line['raw']['highest'] else None
+                k_line['lowest'] = k_line['raw']['lowest'] * qfq_factor if k_line['raw']['lowest'] else None
+            else:
+                # 保持原值
+                pass
+        
+        return k_lines
+    
+    @staticmethod
+    def _find_qfq_factor(target_date: str, sorted_factors: list) -> float:
+        """
+        查找目标日期对应的复权因子
+        
+        算法：找到目标日期所在的时间段，返回该时间段的复权因子
+        时间段定义：大于等于前一个因子日期，小于后一个因子日期
+        
+        Args:
+            target_date: 目标日期 (YYYYMMDD格式)
+            sorted_factors: 按时间升序排列的复权因子列表
+            
+        Returns:
+            float: 复权因子，如果没找到返回None
+        """
+        if not sorted_factors:
+            return None
+            
+        # 如果只有一个因子，直接返回
+        if len(sorted_factors) == 1:
+            return sorted_factors[0]['qfq']
+        
+        # 遍历复权因子，找到目标日期所在的时间段
+        for i in range(len(sorted_factors)):
+            current_factor = sorted_factors[i]
+            current_factor_date = current_factor['date']
+            
+            # 如果是第一个因子，且目标日期小于等于第一个因子日期
+            if i == 0 and target_date <= current_factor_date:
+                return current_factor['qfq']
+            
+            # 如果是最后一个因子，且目标日期大于等于最后一个因子日期
+            if i == len(sorted_factors) - 1 and target_date >= current_factor_date:
+                return current_factor['qfq']
+            
+            # 检查是否在当前因子和下一个因子之间的时间段
+            if i < len(sorted_factors) - 1:
+                next_factor_date = sorted_factors[i + 1]['date']
+                
+                # 时间段：大于等于当前因子日期，小于下一个因子日期
+                if target_date >= current_factor_date and target_date < next_factor_date:
+                    return current_factor['qfq']
+        
+        # 如果没有找到对应的时间段，返回None
+        return None
 
     

aa16bc7e40c935036583ed0b2a0b2e5723f89d33
Garnet Xin <xin_garnet@foxmail.com>
2025-08-27 17:48:46 +0800

    fixed all errors

--- app/analyzer/analyzer_service.py
+++ app/analyzer/analyzer_service.py
@@ -29,36 +29,6 @@ class AnalyzerService:
         return annual_return
 
     @staticmethod
-    def to_usable_stock_idx(stock_idx):
-        if not stock_idx:
-            return []
-            
-        filtered_idx = []
-        
-        for stock in stock_idx:
-            stock_id = stock.get('id', '')
-            stock_name = stock.get('name', '')
-            
-            # 过滤条件1：排除北交所股票（ID包含BJ）
-            avoid_name_starts_with = conf['stock_idx']['avoid_name_starts_with']
-            avoid_code_starts_with = conf['stock_idx']['avoid_code_starts_with']
-            avoid_exchange_center = conf['stock_idx']['avoid_exchange_center']
-
-            if any(stock_name.startswith(prefix) for prefix in avoid_name_starts_with):
-                continue
-
-            if any(stock_id.startswith(prefix) for prefix in avoid_code_starts_with):
-                continue
-
-            if any(string in stock_id for string in avoid_exchange_center):
-                continue
-
-            # 通过所有过滤条件，添加到结果列表
-            filtered_idx.append(stock)
-            
-        return filtered_idx
-
-    @staticmethod
     def find_valleys(daily_data: list, min_drop_threshold: float = 0.10, 
                      local_range_days: int = 5, lookback_days: int = 60) -> list:
         """

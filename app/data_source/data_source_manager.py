34b0b35a59f52c9104f8bba11d7ca0c6700af4b2
Garnet Xin <xin_garnet@foxmail.com>
2025-09-24 15:42:15 +0800

    add price indexes data

--- app/data_source/data_source_manager.py
+++ app/data_source/data_source_manager.py
@@ -35,14 +35,14 @@ class DataSourceManager:
         
         # 然后更新股票指数
         self.latest_stock_index = tu.renew_stock_index(self.latest_market_open_day)
-        logger.info(f"🔍 股票清单更新完成")
+        # logger.info(f"🔍 股票清单更新完成")
 
-        await tu.renew_stock_k_lines(self.latest_market_open_day, self.latest_stock_index)
+        # await tu.renew_stock_k_lines(self.latest_market_open_day, self.latest_stock_index)
         
         # ak.inject_dependency(tu).renew_stock_k_line_factors(self.latest_market_open_day, self.latest_stock_index)
 
         # macro economic indexes
-        # tu.renew_price_indexes(self.latest_market_open_day)
+        tu.renew_price_indexes(self.latest_market_open_day)
 
         # below are not implemented yet
         # tu.renew_global_economic_data()

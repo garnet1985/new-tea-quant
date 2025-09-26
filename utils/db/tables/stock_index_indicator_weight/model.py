8d97f2a01a209dc745588da400876cdcb4623368
Garnet Xin <xin_garnet@foxmail.com>
2025-09-26 12:34:25 +0800

    add stock index indicators

---
+++ utils/db/tables/stock_index_indicator_weight/model.py
@@ -0,0 +1,7 @@
+from utils.db.db_model import BaseTableModel
+
+class StockIndexIndicatorWeightModel(BaseTableModel):
+    def __init__(self, table_name: str, connected_db):
+        super().__init__(table_name, connected_db)
+        # 标记为基础表（不需要前缀）
+        self.is_base_table = True

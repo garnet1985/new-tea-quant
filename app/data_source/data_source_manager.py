544f45f300d63b3238227d66fe3c7393e7156a64
Garnet Xin <xin_garnet@foxmail.com>
2025-09-26 11:25:25 +0800

    add shibor

--- app/data_source/data_source_manager.py
+++ app/data_source/data_source_manager.py
@@ -36,16 +36,21 @@ class DataSourceManager:
         # 然后更新股票指数
         self.latest_stock_index = tu.renew_stock_index(self.latest_market_open_day)
 
-        await tu.renew_stock_k_lines(self.latest_market_open_day, self.latest_stock_index)
+
+        # renew jobs:
+
+        # await tu.renew_stock_k_lines(self.latest_market_open_day, self.latest_stock_index)
         
-        ak.inject_dependency(tu).renew_stock_k_line_factors(self.latest_market_open_day, self.latest_stock_index)
+        # ak.inject_dependency(tu).renew_stock_k_line_factors(self.latest_market_open_day, self.latest_stock_index)
 
         # macro economic indexes
-        tu.renew_price_indexes(self.latest_market_open_day)
+        # tu.renew_price_indexes(self.latest_market_open_day)
 
-        tu.renew_LPR(self.latest_market_open_day)
+        # tu.renew_LPR(self.latest_market_open_day)
 
-        tu.renew_GDP(self.latest_market_open_day)
+        # tu.renew_GDP(self.latest_market_open_day)
+
+        tu.renew_Shibor(self.latest_market_open_day)
 
         # corporate financial data
-        tu.renew_corporate_finance(self.latest_market_open_day)
+        # tu.renew_corporate_finance(self.latest_market_open_day)

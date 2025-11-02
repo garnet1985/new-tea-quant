bf5c2c0a73ccc9d0b88d2dea5d6fc2934168f145
Garnet Xin <xin_garnet@foxmail.com>
2025-11-02 12:25:50 +0800

    add extra actions support for stoploss

--- app/analyzer/components/entity/target.py
+++ app/analyzer/components/entity/target.py
@@ -46,6 +46,19 @@ class InvestmentTarget:
         if 'sell_ratio' not in stage and 'close_invest' not in stage:
             raise ValueError(f"stage must have either 'sell_ratio' or 'close_invest' field: {stage}")
 
+    @staticmethod
+    def create_stage(name: str, target_settings: Dict[str, Any]):
+        stage = {
+            'name': name,
+        }
+        if target_settings.get('ratio', None) is not None:
+            stage['ratio'] = target_settings.get('ratio', 0)
+        if target_settings.get('sell_ratio', None) is not None:
+            stage['sell_ratio'] = target_settings.get('sell_ratio', 0)
+        if target_settings.get('close_invest', None) is not None:
+            stage['close_invest'] = target_settings.get('close_invest', False)
+        return stage
+
 
     def update_amplitude_tracking(self, record_of_today: Dict[str, Any]):
         date = record_of_today.get('date', '')
@@ -65,6 +78,16 @@ class InvestmentTarget:
             self.amplitude_tracking['min_close_reached']['date'] = date
             self.amplitude_tracking['min_close_reached']['ratio'] = (close_price - self.start_record_ref.get('close', 0)) / self.start_record_ref.get('close', 0)
 
+
+    def check(self, record_of_today: Dict[str, Any]):
+        date = record_of_today.get('date', '')
+        last_check_date = self.tracker.get('last_check_date', '')
+        if date <= last_check_date:
+            return
+
+        close_price = record_of_today.get('close', 0)
+        if close_price >= self.content['target_price']:
+            self.content['is_achieved'] = True
 
     def to_dict(self):
         return self.content

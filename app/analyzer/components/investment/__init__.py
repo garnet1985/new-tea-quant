e2c91dca89f833eb1d25150ec4b1518fa745a2ea
Garnet Xin <xin_garnet@foxmail.com>
2025-11-03 14:29:35 +0800

    add flow chart

--- app/analyzer/components/investment/__init__.py
+++ app/analyzer/components/investment/__init__.py
@@ -2,7 +2,6 @@
 投资管理库 - 提供投资目标管理、投资分析等通用功能
 """
 
-from .investment_goal_manager import InvestmentGoalManager
 from .investment_recorder import InvestmentRecorder
 
-__all__ = ['InvestmentGoalManager', 'InvestmentRecorder']
+__all__ = ['InvestmentRecorder']

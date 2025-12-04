7df397d692db2c6fd3afe7b37dcec8d023110c96
Garnet Xin <xin_garnet@foxmail.com>
2025-12-04 11:14:22 +0300

    make db model actively fetch db instance; deleted db manager unused CRUDs

--- app/data_manager/base_tables/gdp/model.py
+++ app/data_manager/base_tables/gdp/model.py
@@ -8,7 +8,7 @@ from utils.db.db_model import BaseTableModel
 class GdpModel(BaseTableModel):
     """GDP数据 Model"""
     
-    def __init__(self, db):
+    def __init__(self, db=None):
         super().__init__('gdp', db)
     
     def load_by_quarter(self, quarter: str) -> Optional[Dict[str, Any]]:
--- app/data_manager/base_tables/price_indexes/model.py
+++ app/data_manager/base_tables/price_indexes/model.py
@@ -8,7 +8,7 @@ from utils.db.db_model import BaseTableModel
 class PriceIndexesModel(BaseTableModel):
     """价格指数 Model"""
     
-    def __init__(self, db):
+    def __init__(self, db=None):
         super().__init__('price_indexes', db)
     
     def load_by_month(self, month: str) -> Optional[Dict[str, Any]]:

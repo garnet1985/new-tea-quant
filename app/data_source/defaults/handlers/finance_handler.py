e516db4dd9d67d32bc2b600e085c70addfdc1ae5
Garnet Xin <xin_garnet@foxmail.com>
2025-12-09 10:41:11 +0300

    complete simple handlers

--- app/data_source/defaults/handlers/finance_handler.py
+++
@@ -1,9 +0,0 @@
-from app.data_source.base_handler import BaseHandler
-from typing import Dict, Any
-
-
-class TushareFinanceHandler(BaseHandler):
-    
-    async def fetch_and_normalize(self, context: Dict[str, Any]) -> Dict:
-        pass
-

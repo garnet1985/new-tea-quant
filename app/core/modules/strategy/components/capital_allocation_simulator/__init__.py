3f49e646b9b42391e7a2e0ed54293507116b8316
Garnet Xin <xin_garnet@foxmail.com>
2026-01-12 20:41:17 +0800

    complete settings design for capital allocator

---
+++ app/core/modules/strategy/components/capital_allocation_simulator/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Capital Allocation Simulator
+
+资金分配型模拟器，在真实资金约束下对枚举器 SOT 结果进行全市场回放。
+"""
+
+from .capital_allocation_simulator_config import CapitalAllocationSimulatorConfig
+from .version_manager import CapitalAllocationSimulationVersionManager
+
+__all__ = [
+    "CapitalAllocationSimulatorConfig",
+    "CapitalAllocationSimulationVersionManager",
+]

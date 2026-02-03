a6b620816622925c12ee486f25116fc395bd94bf
Garnet Xin <xin_garnet@foxmail.com>
2026-02-03 17:21:38 +0800

    apply new date util to all non-entity based data source

--- userspace/data_source/handlers/corporate_finance/config.py
+++ userspace/data_source/handlers/corporate_finance/config.py
@@ -1,17 +1,20 @@
 """
 Corporate Finance Handler 配置。绑定表 sys_corporate_finance。
 """
+from core.utils.date import DateUtils
+
+
 CONFIG = {
     "table": "sys_corporate_finance",
     "renew": {
         "type": "rolling",
         "rolling": {
-            "unit": "quarterly",
+            "unit": DateUtils.PERIOD_QUARTER,
             "length": 3,
         },
         "last_update_info": {
             "date_field": "quarter",
-            "date_format": "quarterly",
+            "date_format": DateUtils.PERIOD_QUARTER,
         },
         "result_group_by": {
             "list": "stock_list",
